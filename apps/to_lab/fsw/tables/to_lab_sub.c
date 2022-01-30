d999d72c9f62d2643c6b5f49de9e305e6fa12023
Jeff St. Jean <jeffrey@stjean.ca>
2022-01-30 00:03:27 -0500

    Add to_lab to Yamcs

--- apps/to_lab/fsw/tables/to_lab_sub.c
+++ apps/to_lab/fsw/tables/to_lab_sub.c
@@ -43,6 +43,7 @@
 #include "ci_lab_msgids.h"
 
 #include "sample_app_msgids.h"
+#include "hw_msgids.h"
 
 #if 0
 #include "hs_msgids.h"
@@ -57,6 +58,7 @@ TO_LAB_Subs_t TO_LAB_Subs = {.Subs = {/* CFS App Subscriptions */
                                       {CFE_SB_MSGID_WRAP_VALUE(TO_LAB_DATA_TYPES_MID), {0, 0}, 4},
                                       {CFE_SB_MSGID_WRAP_VALUE(CI_LAB_HK_TLM_MID), {0, 0}, 4},
                                       {CFE_SB_MSGID_WRAP_VALUE(SAMPLE_APP_HK_TLM_MID), {0, 0}, 4},
+                                      {CFE_SB_MSGID_WRAP_VALUE(HW_SEND_HK_MID), {0, 0}, 4},
 
 #if 0
         /* Add these if needed */
